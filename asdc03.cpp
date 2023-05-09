ca32364f2b887d2c0eb1fc3bab7e4ee2e16e2161
KaterinaSpinu <126262145+KaterinaSpinu@users.noreply.github.com>
2023-05-09 19:09:51 +0300

    Add files via upload

---
+++ asdc03.cpp
@@ -0,0 +1,177 @@
+﻿#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
+#include <fstream>
+#include <atlstr.h>
+#include <stdio.h>
+
+using namespace std;
+
+struct base
+{
+    int nr;
+    char name[20];
+    char description[25];
+    double price;
+    int count;
+    char unit[5];
+    long long int id;
+    base* b;
+};
+
+void print()
+{
+    FILE* file = fopen("mebeli1.txt", "rt");
+    char str[100];
+    while (!feof(file))
+    {
+        if (!fgets(str, 100, file)) break;
+        puts(str);
+    }
+    fclose(file);
+}
+
+
+void addvd(base* m)
+{
+    int t;
+    cout << "n = ";
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cout << "Name: ";
+        cin >> m[i].name;
+        cout << "Description: ";
+        cin >> m[i].description;
+        cout << "Price: ";
+        cin >> m[i].price;
+        cout << "Count: ";
+        cin >> m[i].count;
+        cout << "Unit: ";
+        cin >> m[i].unit;
+        cout << "Id: ";
+        cin >> m[i].id;
+    }
+
+    fstream f("mebeli1.txt", ios::out);
+    for (int i = 0; i < t; i++)
+    {
+        for (int i = 0; !f.eof(); i++)
+        {
+            char temp[20];
+            f >> temp;
+            if (f.eof()) break;
+            f >> m[i].name;
+            cout << m[i].name << " ";
+            f >> m[i].description;
+            cout << m[i].description << " ";
+            f >> m[i].price;
+            cout << m[i].price << " ";
+            f >> m[i].count;
+            cout << m[i].count << " ";
+            f >> m[i].unit;
+            cout << m[i].unit << " ";
+            f >> m[i].id;
+            cout << m[i].id << "\n ";
+        }
+        f.close();
+    }
+}
+
+class list
+{
+private:
+    base* head, * tail;
+public:
+    list()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+    void display()
+    {
+
+        base* temp = new base;
+        temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->nr << "\t";
+            temp = temp->b;
+        }
+    }
+    void insert_start(int value)
+    {
+        base* temp = new base;
+        temp->nr = value;
+        temp->b = head;
+        head = temp;
+    }
+    void insert_position(int pos, int value)
+    {
+        base* pre = new base;
+        base* cur = new base;
+        base* temp = new base;
+        cur = head;
+        for (int i = 1; i < pos; i++)
+        {
+            pre = cur;
+            cur = cur->b;
+        }
+        temp->nr = value;
+        pre->b = temp;
+        temp->b = cur;
+    }
+    void delete_first()
+    {
+        base* temp = new base;
+        temp = head;
+        head = head->b;
+        delete temp;
+    }
+    void delete_last()
+    {
+        base* current = new base;
+        base* previous = new base;
+        current = head;
+        while (current->b != NULL)
+        {
+            previous = current;
+            current = current->b;
+        }
+        tail = previous;
+        previous->b = NULL;
+        delete current;
+    }
+    void delete_position(int pos)
+    {
+        base* current = new base;
+        base* previous = new base;
+        current = head;
+        for (int i = 1; i < pos; i++)
+        {
+            previous = current;
+            current = current->b;
+        }
+        previous->b = current->b;
+    }
+};
+
+int main()
+{
+    int k;
+    base* p1 = new base[100];
+    do {
+        cout << "1. Enter data\n";
+        cout << "2. Show data\n";
+        cout << "3. Delete\n";
+        cout << "5. Exit\n";
+        cout << "==>";
+        cin >> k;
+        switch (k) {
+        case 1: addvd(p1); break;
+        case 2: print(); break;
+        case 3: cout << ""; break;
+        }
+    } while (k != 4);
+    system("pause");
+    return 0;
+}
---
+++ asdc03_binary_tree.cpp
@@ -0,0 +1,119 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+const int maxSize = 60;
+struct base
+{
+    char name[maxSize];
+    char description[maxSize];
+    double price;
+    int count;
+    char unit[maxSize];
+    long long int id;
+    base* next;
+};
+base* b;
+void Input(char name[maxSize], char description[maxSize], double& price, int& count, char unit[maxSize], long long int& id)
+{
+    cout << "name = ";
+    cin.get();
+    cin.get(name, sizeof(name));
+    cout << "description = ";
+    cin.get();
+    cin.get(description, sizeof(description));
+    cout << "price = ";
+    cin >> price;
+    cout << "count = ";
+    cin >> count;
+    cout << "unit = ";
+    cin.get();
+    cin.get(unit, sizeof(unit));
+    cout << "id = ";
+    cin >> id;
+    cout << endl;
+}
+
+void Print(base* b)
+{
+    base* j = b;
+    while (j == NULL);
+    {
+        cout << "Mebeli: " << j->name << ";" << j->description << ";" << j->price << ";" << j->count << ";" << j->unit << ";" << j->id << ";";
+        j = j->next;
+    }
+    cout << endl;
+
+}
+
+base* Search(base* b, double price)
+{
+    base* p = b;
+    while (p != NULL)
+    {
+        if (p->price == price)
+            break;
+        p = p->next;
+    }5
+    return p;
+}
+
+void Delete(base*& pbeg, base* pos)
+{
+    if (pbeg == NULL || pos == NULL)
+        return;
+    if (pos == pbeg)
+    {
+        pbeg = pbeg->next;
+        delete pos;
+    }
+    else
+    {
+        base* prev = pbeg;
+        while (prev != NULL && prev->next != pos)
+            prev = prev->next;
+        if (prev != NULL)
+        {
+            prev->next = pos->next;
+            delete pos;
+        }
+    }
+}
+
+void Free(base*& pbeg)
+{
+    base* p;
+    while (pbeg != NULL)
+    {
+        p = pbeg;
+        pbeg = pbeg->next;
+        delete p;
+    }
+}
+
+int main()
+{
+    char name[maxSize] = { '\0' };
+    char description[maxSize] = { '\0' };
+    double price;
+    int count;
+    char unit[maxSize] = { '\0' };
+    long long int id;
+
+    base* b;
+    int n;
+
+    cout << "n = ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        Input(name, description, price, count, unit, id);
+    }
+
+    Print(b);
+
+    Free(b);
+
+    system("pause");
+    return 0;
+}
+
